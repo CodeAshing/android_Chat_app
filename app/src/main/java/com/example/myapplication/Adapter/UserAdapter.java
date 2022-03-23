602cfeea743aacb6c3ba9e21cd948ec1d0ab62f4
Asharib Ahmed <asharibahmed143@gmail.com>
2022-03-24 02:44:25 +0500

    first commit

---
+++ app/src/main/java/com/example/myapplication/Adapter/UserAdapter.java
@@ -0,0 +1,78 @@
+package com.example.myapplication.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.helper.widget.Layer;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.myapplication.MessageActivity;
+import com.example.myapplication.Model.User;
+import com.example.myapplication.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context nContext;
+    private List<User> nUsers;
+
+    public UserAdapter(Context nContext, List<User> nUsers){
+        this.nUsers = nUsers;
+        this.nContext = nContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(nContext).inflate(R.layout.user_item,  parent, false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        User user = nUsers.get(position);
+        holder.username.setText(user.getUsername());
+        if(user.getImageURL().equals("default")){
+            holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+        }else{
+            Glide.with(nContext).load(user.getImageURL()).into(holder.profile_image);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(nContext, MessageActivity.class);
+                intent.putExtra("userid", user.getId());
+                nContext.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return nUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView username;
+        public ImageView profile_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+        }
+    }
+}
